d950c9f6b9864d0e5bdb3772e0e2a5a559795cb6
Samantha Callie <Hoomee90@gmail.com>
2023-08-17 15:49:49 -0700

    create js file with comments detailing how form results will change output

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// Name: Name, dunkass
+
+/* Soul Type: Skills
+Domination
+Endless - persuasion
+Enlightened - magic
+Tormented - history
+Blank - medicine
+Caressing - mechanics
+Chaotic - commanding
+Changeling - occultism
+Decrepit - thievery
+Heartless - wealth
+Latent - a little bit of everything
+Radiating - instincts
+Shadowed - botany
+Solitary - weapons
+Suffocated - cooking
+Tainted - brawling
+*/
+
+// Ability + Weakness: Useful power, and how your weakness keeps you from taking full advantage of it
+
+/* DoB: Lifelong Struggle
+Aries March 21 - April 19
+Taurus April 20 – May 20
+Gemini May 21- June 21
+Cancer June 22- July 22
+Leo July 23 – August 22
+Virgo August 23 – September 22
+Libra September 23 – October 23
+Scorpio October 24 – November 21
+Sagittarius November 22 – December 21
+Capricorn December 22 – January 19
+Aquarius January 20 – February 18
+Pisces February 19 – March 20
+*/
+
+/* Color: Personality
+XX - any non 00 value
+xx - any non 00 value < XX
+
+XX0000 - mysterious and composed
+XXxx00 - earnest and humble
+XXXX00 - acerbic and cynical
+xxXX00 - playful and impish
+00XX00 - outspoken and radical
+00XXxx - gentle and dependable 
+00XXXX - zealous and principled
+00xxXX - performative and dramatic
+0000XX - rigid and deferential 
+xx00XX - whimsical and relaxed
+XX00XX - imperious and arrogant
+XX00xx - exuberant and rollicking
+*/
