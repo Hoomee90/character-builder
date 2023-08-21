c696606fcb94039ffa4b0da5628ae024403617db
Samantha Callie <Hoomee90@gmail.com>
2023-08-21 13:30:48 -0700

    add dictionary linking color form output to strings of personality traits

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ Radiating - instincts
 Shadowed - botany
 Solitary - weapons
 Suffocated - cooking
-Tainted - brawling*/
+Tainted - brawling */
 
 // Ability + Weakness: Useful power, and how your weakness keeps you from taking full advantage of it
 
@@ -33,32 +33,35 @@ Sagittarius November 22 – December 21 - impotent irrelevance
 Capricorn December 22 – January 19 - excessive limitations
 Aquarius January 20 – February 18 - unwavering stubbornness
 Pisces February 19 – March 20 - blinding idolization
- 
+
 Color: Personality
 XX - any non 00 value
 xx - any non 00 value < XX
 
-0-0-0 1-0-0
-2-1-0 2-0-0
-2-2-0 1-1-0
-1-2-0 1--0
-0-2-0 0-1-0
-0-2-1 0-1-1
-0-2-2 0-1-2
-0-1-2 0-1-1
-0-0-2 0-0-1
-0-0-2 0-0-1
-2-0-2 1-0-2
-2-0-1 1-0-1
+2-0-0 2-1-1 x
+2-1-0 2-2-1 x
+2-2-0 1-1-0 1-1-1 x
+1-2-0 0-1-0 x
+0-2-0 1-2-1 x
+0-2-1 1-2-2 x
+0-2-2 0-1-1 2-2-2 x
+0-1-2 0-0-1 x
+0-0-2 1-1-2 x
+1-0-2 2-1-2 0-0-0 x
+2-0-2 1-0-1
+2-0-1 1-0-0
 
-XX0000 - mysterious and composed 
-XXxx00 - earnest and humble 
+
+
+
+XX0000 - mysterious and composed
+XXxx00 - earnest and humble
 XXXX00 - acerbic and cynical  
 xxXX00 - playful and impish
 00XX00 - outspoken and radical
 00XXxx - gentle and dependable 
 00XXXX - zealous and principled
-00xxXX - performative and dramatic
+00xxXX - performative and dramatic 
 0000XX - rigid and deferential 
 xx00XX - whimsical and relaxed
 XX00XX - imperious and arrogant
@@ -67,43 +70,6 @@ XX00xx - exuberant and rollicking
 const assignment = {
   "0-0-1": "XX0000",
   "0-0-2": "XX0000",
-  // ... continue for other non-zero values in the first position only
-
-  "2-1-0": "XXxx00",
-  "3-1-0": "XXxx00",
-  "3-2-0": "XXxx00",
-  // ... continue for other similar patterns
-
-  "2-2-0": "XXXX00",
-  "3-2-0": "XXXX00",
-  // ... and so on
-
-  "1-2-0": "xxXX00",
-  // ...
-
-  "0-1-0": "00XX00",
-  // ...
-
-  "0-2-1": "00XXxx",
-  // ...
-
-  "0-2-2": "00XXXX",
-  // ...
-
-  "0-1-2": "00xxXX",
-  // ...
-
-  "0-0-3": "0000XX",
-  // ...
-
-  "1-0-2": "xx00XX",
-  // ...
-
-  "2-0-2": "XX00XX",
-  // ...
-
-  "2-0-1": "XX00xx",
-  // ...
 */
 
 const thresholds = [85, 170, 255];
@@ -115,6 +81,7 @@ function getRange(value) {
 }
 
 function getColorBucket(RGB) {
+  //turn RGB into one of 27 buckets
   const [r, g, b] = RGB;
   const rRange = getRange(r);
   const gRange = getRange(g);
@@ -137,8 +104,34 @@ function colorSubmissionHandler(colorHEX) {
   const colorRGB = hexToRGB(colorHEX);
   const colorBucket = getColorBucket(colorRGB);
   const bucketPersonalityPairs = {
-      "high-high-high": "acerbic and cynical"
-  };
+      "high-low-low": "mysterious and composed",
+      "high-med-med": "mysterious and composed",
+      "high-med-low": "earnest and humble",
+      "high-high-med": "earnest and humble",
+      "high-high-low": "acerbic and cynical",
+      "med-med-low": "acerbic and cynical",
+      "med-med-med": "acerbic and cynical",
+      "med-high-low": "playful and impish",
+      "low-med-low": "playful and impish",
+      "low-high-low": "outspoken and radical",
+      "med-high-med": "outspoken and radical",
+      "low-high-med": "gentle and dependable",
+      "med-high-high": "gentle and dependable",
+      "low-high-high": "zealous and principled",
+      "low-med-med": "zealous and principled",
+      "high-high-high": "zealous and principled",
+      "low-med-high": "performative and dramatic",
+      "low-low-med": "performative and dramatic",
+      "low-low-high": "rigid and deferential",
+      "med-med-high": "rigid and deferential",
+      "med-low-high": "whimsical and relaxed",
+      "high-med-high": "whimsical and relaxed",
+      "low-low-low": "whimsical and relaxed",
+      "high-low-high": "imperious and arrogant",
+      "med-low-med": "imperious and arrogant",
+      "high-low-med": "exuberant and rollicking",
+      "med-low-low": "exuberant and rollicking"
+    };
 
   console.log(bucketPersonalityPairs[colorBucket]);
 }
