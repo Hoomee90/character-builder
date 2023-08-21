32ff88bf3c088cd2429fd69dd5dd1105701e7fe8
Samantha Callie <Hoomee90@gmail.com>
2023-08-21 14:19:42 -0700

    add date submission handler to collect and convert form data into lifelong stuggles

--- js/scripts.js
+++ js/scripts.js
@@ -20,57 +20,47 @@ Tainted - brawling */
 
 // Ability + Weakness: Useful power, and how your weakness keeps you from taking full advantage of it
 
-/* DoB: Lifelong Struggle
+/* DoB: Lifelong Struggle WIP
 Aries March 21 - April 19 - unnecessary suffering
 Taurus April 20 – May 20 - vacant directionlessness
 Gemini May 21- June 21 - bitter nihilism
 Cancer June 22- July 22 - overwhelming self-doubt
-Leo July 23 – August 22 - unhealthy attachments
-Virgo August 23 – September 22 - positive connection
-Libra September 23 – October 23 - being seen
-Scorpio October 24 – November 21 - ambition and hubris
-Sagittarius November 22 – December 21 - impotent irrelevance 
-Capricorn December 22 – January 19 - excessive limitations
+Leo July 23 – August 22 - unhealthy attachment
+Virgo August 23 – September 22 - lonely isolation
+Libra September 23 – October 23 - compulsive insincerity
+Scorpio October 24 – November 21 - hubristic ambition
+Sagittarius November 22 – December 21 - ineffective irrelevance
+Capricorn December 22 – January 19 - excessive restrictions
 Aquarius January 20 – February 18 - unwavering stubbornness
 Pisces February 19 – March 20 - blinding idolization
 
-Color: Personality
-XX - any non 00 value
-xx - any non 00 value < XX
-
-2-0-0 2-1-1 x
-2-1-0 2-2-1 x
-2-2-0 1-1-0 1-1-1 x
-1-2-0 0-1-0 x
-0-2-0 1-2-1 x
-0-2-1 1-2-2 x
-0-2-2 0-1-1 2-2-2 x
-0-1-2 0-0-1 x
-0-0-2 1-1-2 x
-1-0-2 2-1-2 0-0-0 x
-2-0-2 1-0-1
-2-0-1 1-0-0
-
-
-
-
-XX0000 - mysterious and composed
-XXxx00 - earnest and humble
-XXXX00 - acerbic and cynical  
-xxXX00 - playful and impish
-00XX00 - outspoken and radical
-00XXxx - gentle and dependable 
-00XXXX - zealous and principled
-00xxXX - performative and dramatic 
-0000XX - rigid and deferential 
-xx00XX - whimsical and relaxed
-XX00XX - imperious and arrogant
-XX00xx - exuberant and rollicking
-
-const assignment = {
-  "0-0-1": "XX0000",
-  "0-0-2": "XX0000",
 */
+
+function dateSubmissionHandler(dateString) {
+  
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.getUTCMonth() + 1;
+
+  const zodiacs = {
+      "excessive restrictions": () => (month == 12 && day >= 22) || (month == 1 && day <= 19),
+      'Aquarius': () => (month == 1 && day >= 20) || (month == 2 && day <= 18),
+      'Pisces': () => (month == 2 && day >= 19) || (month == 3 && day <= 20),
+      "unnecessary suffering": () => (month == 3 && day >= 21) || (month == 4 && day <= 19),
+      "vacant directionlessness": () => (month == 4 && day >= 20) || (month == 5 && day <= 20),
+      "bitter nihilism": () => (month == 5 && day >= 21) || (month == 6 && day <= 20),
+      "overwhelming self-doubt": () => (month == 6 && day >= 21) || (month == 7 && day <= 22),
+      "unhealthy attachment": () => (month == 7 && day >= 23) || (month == 8 && day <= 22),
+      "lonely isolation": () => (month == 8 && day >= 23) || (month == 9 && day <= 22),
+      "compulsive insincerity": () => (month == 9 && day >= 23) || (month == 10 && day <= 22),
+      "hubristic ambition": () => (month == 10 && day >= 23) || (month == 11 && day <= 21),
+      "ineffective irrelevance": () => (month == 11 && day >= 22) || (month == 12 && day <= 21),
+  };
+
+  for (let sign in zodiacs) {
+      if (zodiacs[sign]()) console.log(sign);
+  }
+}
 
 const thresholds = [85, 170, 255];
 
@@ -103,6 +93,7 @@ function hexToRGB(hex) {
 function colorSubmissionHandler(colorHEX) {
   const colorRGB = hexToRGB(colorHEX);
   const colorBucket = getColorBucket(colorRGB);
+  //dictionary for how RGB relate to personality"
   const bucketPersonalityPairs = {
       "high-low-low": "mysterious and composed",
       "high-med-med": "mysterious and composed",
@@ -110,7 +101,7 @@ function colorSubmissionHandler(colorHEX) {
       "high-high-med": "earnest and humble",
       "high-high-low": "acerbic and cynical",
       "med-med-low": "acerbic and cynical",
-      "med-med-med": "acerbic and cynical",
+      "high-high-high": "acerbic and cynical",
       "med-high-low": "playful and impish",
       "low-med-low": "playful and impish",
       "low-high-low": "outspoken and radical",
@@ -119,14 +110,14 @@ function colorSubmissionHandler(colorHEX) {
       "med-high-high": "gentle and dependable",
       "low-high-high": "zealous and principled",
       "low-med-med": "zealous and principled",
-      "high-high-high": "zealous and principled",
+      "low-low-low": "zealous and principled",
       "low-med-high": "performative and dramatic",
       "low-low-med": "performative and dramatic",
       "low-low-high": "rigid and deferential",
       "med-med-high": "rigid and deferential",
       "med-low-high": "whimsical and relaxed",
       "high-med-high": "whimsical and relaxed",
-      "low-low-low": "whimsical and relaxed",
+      "med-med-med": "whimsical and relaxed",
       "high-low-high": "imperious and arrogant",
       "med-low-med": "imperious and arrogant",
       "high-low-med": "exuberant and rollicking",
@@ -142,7 +133,9 @@ window.addEventListener("load", function() {
   form.addEventListener("submit", function(event) {
     event.preventDefault();
     const colorInput = document.querySelector("#color").value;
-
+    const dateInput = document.querySelector("#born").value;
+    
+    dateSubmissionHandler(dateInput);
     colorSubmissionHandler(colorInput);
   });
 });
